d69866f3a37d0194bbe729eb8d75063e11a936f7
Arthur Brasa <arthurbrasa@outlook.com>
2022-02-06 23:27:28 -0300

    Queue

--- Semana_3_pilha_fila_deque_lista/queue.py
+++ Semana_3_pilha_fila_deque_lista/queue.py
@@ -1,9 +1,14 @@
+from time import sleep
+
 class Queue:
     def __init__(self):
         self.items = []
 
     def isEmpty(self):
         return self.items == []
+
+    def __str__(self) -> str:
+        return str(self.items)
 
     def enqueue(self, item):
         self.items.insert(0,item)
@@ -13,3 +18,24 @@ class Queue:
         
     def size(self):
         return len(self.items)
+
+
+""" Exemplo HotPotato"""
+
+def hotPotato(listName, num):
+    fila = Queue()
+    for name in listName:
+        fila.enqueue(name)
+    
+    while fila.size() > 1:
+        for i in range(num):
+            fila.enqueue(fila.dequeue())
+        fila.dequeue()
+
+    return fila.dequeue()    
+
+nomesLista = ['Arthur', 'Marcos', 'Maria', 'Sakura', 'Naruto', 'Covid-19']
+
+
+print(hotPotato(nomesLista, 5))
+
